0dc6ff2b0bcbc80c963449f7fe90f8631b13e0a0
nailasalmayusroini <nailasalmayusroini@mail.ugm.ac.id>
2024-09-27 08:21:00 +0000

    y

--- Problem1/fizzBuzzTest.cpp
+++ Problem1/fizzBuzzTest.cpp
@@ -7,7 +7,7 @@ int main() {
 
 vector<string> expectedResult = {
     "1", "2", "Fizz", "4", "Buzz", "Fizz", "7", "8", "Fizz", "Buzz",
-    "11", "Fizz", "13", "14", "FizzBuzz", "Fizz", "17", "Fizz", "19", "Buzz",
+    "11", "Fizz", "13", "14", "FizzBuzz", "16", "17", "Fizz", "19", "Buzz",
     "Fizz", "22", "23", "Fizz", "Buzz", "26", "Fizz", "28", "29", "FizzBuzz",
     "31", "32", "Fizz", "34", "FizzBuzz", "Fizz", "37", "38", "Fizz", "Buzz",
     "41", "Fizz", "43", "44", "Buzz", "46", "47", "Fizz", "49", "Buzz",
