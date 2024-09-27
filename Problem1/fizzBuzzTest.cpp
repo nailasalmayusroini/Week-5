2b34859049ea9fe5b2c3b50f4f27b392952ca599
nailasalmayusroini <nailasalmayusroini@mail.ugm.ac.id>
2024-09-27 08:26:34 +0000

    test

--- Problem1/fizzBuzzTest.cpp
+++ Problem1/fizzBuzzTest.cpp
@@ -22,32 +22,31 @@ vector<string> fizzbuzzResult(100);
 
 // Loop from 1 to 100
     for(int i = 1; i <= 100; i++){
-        if (i % 3 == 0 && i % 5 == 0) { // for every multiples of three and five, the output will be "FizzBuzz"
+        if (i % 3 == 0 && i % 5 == 0) { // for multiples of three and five
             fizzbuzzResult[i-1] = "FizzBuzz";   
         }
-        else if (i % 5 == 0) { // for every multiples of five, the output will be "Buzz"
+        else if (i % 5 == 0) { // for multiples of five
             fizzbuzzResult[i-1] = "Buzz";
         }
-        else if (i % 3 == 0) { // for every multiples of three, the output will be "Fizz"
+        else if (i % 3 == 0) { // for multiples of three
             fizzbuzzResult[i-1] = "Fizz";
         }
-        else { // the output will be an order of 1 to 100 except the ones that are multiples of three and five
+        else { // for non-multiples of three or five
             fizzbuzzResult[i-1] = to_string(i);
         }
-        
     }
     
-    // the final output
+    // Print the final output
     for (int i = 0; i < 100; i++) {
         cout << fizzbuzzResult[i] << endl;
     }
-        
 
+    // Check if the test passes
     bool testPassed = true;
     for (int i = 0; i < 100; i++) {
         if (expectedResult[i] != fizzbuzzResult[i]) {
-        testPassed = false;
-        break;
+            testPassed = false;
+            break;
         }
     }
 
@@ -60,4 +59,3 @@ vector<string> fizzbuzzResult(100);
 
     return 0;
 }
-
